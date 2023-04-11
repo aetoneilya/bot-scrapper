4b8c11932cbaba0de2919bd988d58acf8a253edc
илья удалов <udalov216@gmail.com>
2023-04-12 00:03:11 +0700

    fix: replaced tests in bot and link parser

--- bot/src/test/java/ru/tinkoff/bot/telegram/command/ListCommandTest.java
+++ bot/src/test/java/ru/tinkoff/bot/telegram/command/ListCommandTest.java
@@ -2,6 +2,7 @@ package ru.tinkoff.bot.telegram.command;
 
 import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.model.Update;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.tinkoff.bot.client.scrapper.ScrapperClient;
 import ru.tinkoff.bot.client.scrapper.dto.response.LinkResponse;
@@ -11,54 +12,62 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ListCommandTest {
-    private final ListLinksResponse listLinksResponseUser0 = new ListLinksResponse(
-            List.of(
-                    new LinkResponse(1, new URI("https://google.com")),
-                    new LinkResponse(2, new URI("https://bing.com")),
-                    new LinkResponse(2, new URI("https://opera.com")),
-                    new LinkResponse(3, new URI("https://yandex.com"))), 4);
-    private final ListLinksResponse listLinksResponseUser1 = new ListLinksResponse(
-            List.of(
-                    new LinkResponse(1, new URI("https://google.com")),
-                    new LinkResponse(3, new URI("https://yandex.com"))), 2);
-    private final ListLinksResponse listLinksResponseUser2 = new ListLinksResponse(List.of(), 0);
-    private final ListCommand listCommand;
-
-    ListCommandTest() throws URISyntaxException {
-        ScrapperClient scrapperClient = mock(ScrapperClient.class);
-        when(scrapperClient.getAllLinks(0)).thenReturn(listLinksResponseUser0);
-        when(scrapperClient.getAllLinks(1)).thenReturn(listLinksResponseUser1);
-        when(scrapperClient.getAllLinks(2)).thenReturn(listLinksResponseUser2);
-        listCommand = new ListCommand(scrapperClient);
-    }
+    ScrapperClient scrapperClient = mock(ScrapperClient.class);
+    private final ListCommand listCommand = new ListCommand(scrapperClient);
 
     @Test
-    void testMessageText() {
-        correctSendMessage(0L, listLinksResponseUser0);
-        correctSendMessage(1L, listLinksResponseUser1);
-        correctSendMessage(2L, listLinksResponseUser2);
-    }
-
-    void correctSendMessage(Long userId, ListLinksResponse response) {
-        Update update = BotUtils.parseUpdate("{\"message\"={\"text\"=\"/list\",\"chat\"={\"id\"=" + userId + "}}}");
+    void listFourLinks() throws URISyntaxException {
+        ListLinksResponse listLinksResponseUser0 = new ListLinksResponse(
+                List.of(
+                        new LinkResponse(1, new URI("https://google.com")),
+                        new LinkResponse(2, new URI("https://bing.com")),
+                        new LinkResponse(2, new URI("https://opera.com")),
+                        new LinkResponse(3, new URI("https://yandex.com"))), 4);
+        when(scrapperClient.getAllLinks(0)).thenReturn(listLinksResponseUser0);
+        String expected = """
+                Tracked links:
+                https://google.com
+                https://bing.com
+                https://opera.com
+                https://yandex.com""";
+        Update update = BotUtils.parseUpdate("{\"message\"={\"text\"=\"/list\",\"chat\"={\"id\"= 0}}}");
 
         String answer = (String) listCommand.handle(update).getParameters().get("text");
 
-
-        if (response.size() > 0) {
-            StringBuilder sendMessageText = new StringBuilder("Tracked links:");
-            for (LinkResponse linkResponse : response.links()) {
-                sendMessageText.append("\n").append(linkResponse.url().toString());
-            }
-            assertEquals(answer, sendMessageText.toString());
-        } else {
-            assertEquals(answer, "No tracked links yet..\nFeel free to add some");
-        }
+        Assertions.assertEquals(expected, answer);
     }
 
+    @Test
+    void listTwoLinks() throws URISyntaxException {
+        ListLinksResponse listLinksResponseUser1 = new ListLinksResponse(
+                List.of(
+                        new LinkResponse(1, new URI("https://google.com")),
+                        new LinkResponse(3, new URI("https://yandex.com"))), 2);
+        when(scrapperClient.getAllLinks(1)).thenReturn(listLinksResponseUser1);
+        String expected = """
+                Tracked links:
+                https://google.com
+                https://yandex.com""";
+        Update update = BotUtils.parseUpdate("{\"message\"={\"text\"=\"/list\",\"chat\"={\"id\"= 1}}}");
+
+        String answer = (String) listCommand.handle(update).getParameters().get("text");
+
+        Assertions.assertEquals(expected, answer);
+    }
+
+    @Test
+    void listEmptyResponse() {
+        ListLinksResponse listLinksResponseUser2 = new ListLinksResponse(List.of(), 0);
+        when(scrapperClient.getAllLinks(2)).thenReturn(listLinksResponseUser2);
+        String expected = "No tracked links yet..\nFeel free to add some";
+        Update update = BotUtils.parseUpdate("{\"message\"={\"text\"=\"/list\",\"chat\"={\"id\"= 2}}}");
+
+        String answer = (String) listCommand.handle(update).getParameters().get("text");
+
+        Assertions.assertEquals(expected, answer);
+    }
 }
