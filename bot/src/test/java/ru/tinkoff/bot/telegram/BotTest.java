a5986e229bb7211f66710602506bcbc72d62b738
илья удалов <udalov216@gmail.com>
2023-05-16 16:47:13 +0700

    mock message counter in tests

--- bot/src/test/java/ru/tinkoff/bot/telegram/BotTest.java
+++ bot/src/test/java/ru/tinkoff/bot/telegram/BotTest.java
@@ -4,16 +4,18 @@ import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.junit.jupiter.api.Test;
+import ru.tinkoff.bot.metric.MessageCounter;
 import ru.tinkoff.bot.telegram.command.StartCommand;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 class BotTest {
     @Test
     void unknownCommand() {
-        Bot bot = new Bot(null, List.of(new StartCommand(null)));
+        Bot bot = new Bot(null, List.of(new StartCommand(null)), mock(MessageCounter.class));
         Update update = BotUtils.parseUpdate("{\"message\"={\"text\"=\"/unknownCommand\",\"chat\"={\"id\"=0}}}");
 
         SendMessage msg = bot.processUpdate(update);
